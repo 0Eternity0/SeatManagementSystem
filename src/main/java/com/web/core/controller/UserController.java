9f6e16b632644d58d36f25a2a0c698cc4a04726a
胡思荣 <749963470@qq.com>
2019-04-03 19:07:59 +0800

    修改了一个类名

--- src/main/java/com/web/core/controller/UserController.java
+++ src/main/java/com/web/core/controller/UserController.java
@@ -4,10 +4,8 @@ package com.web.core.controller;
 
 import com.web.core.pojo.User;
 import com.web.core.service.UserService;
-import com.web.core.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
